259c1c30661ba4f252f471c95ab2f5dddf071ced
Marcio <marciopereiraaraujo1997d1@gmail.com>
2026-06-04 17:49:37 -0300

    feat(backend-brand): retrive products based on brand

--- backend/src/main/java/edu/marcio/stock/controller/ProductBrandController.java
+++ backend/src/main/java/edu/marcio/stock/controller/ProductBrandController.java
@@ -1,18 +1,26 @@
 package edu.marcio.stock.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.marcio.stock.dto.productBrand.ProductBrandRequest;
+import edu.marcio.stock.entity.Product;
 import edu.marcio.stock.entity.ProductBrand;
 import edu.marcio.stock.service.ProductBrandService;
+import edu.marcio.stock.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,11 +28,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ProductBrandController {
 
     private final ProductBrandService productBrandService;
+    private final ProductService productService;
 
     @PostMapping
     public ResponseEntity<ProductBrand> registerNewBrand(@RequestBody @Valid ProductBrandRequest body) {
         ProductBrand brand = productBrandService.registeBrand(body);
         return ResponseEntity.status(HttpStatus.CREATED).body(brand);
+    }
+
+    @GetMapping("/{id}/products")
+    public ResponseEntity<Page<Product>> getMethodName(
+            @PathVariable String id,
+            @RequestParam(required = false) String name,
+            @PageableDefault(sort = "name") Pageable pageable) {
+
+        Page<Product> productPage = productService.getProductsFromBrand(id, name, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(productPage);
     }
 
 }
--- backend/src/main/java/edu/marcio/stock/repository/ProductRepository.java
+++ backend/src/main/java/edu/marcio/stock/repository/ProductRepository.java
@@ -12,21 +12,29 @@ import edu.marcio.stock.entity.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 
-    @Query("SELECT DISTINCT p FROM Product p " +
-            "JOIN p.sectors s " +
-            "LEFT JOIN p.brand b " +
-            "WHERE s.id = :id " +
-            "AND (:productName IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :productName, '%'))) " +
-            "AND (:brandId IS NULL OR b.id = :brandId) " +
-            "AND (:sku IS NULL OR p.sku = :sku) " +
-            "AND (:ean IS NULL OR p.ean = :ean) " +
-            "AND (:isActive IS NULL OR p.isActive = :isActive)")
-    Page<Product> findAllProductsBySectorIdWithFilters(
-            Pageable pageable,
-            @Param("id") String id,
-            @Param("productName") String productName,
-            @Param("sku") String sku,
-            @Param("ean") String ean,
-            @Param("brandId") String brandId,
-            @Param("isActive") boolean isActive);
+        @Query("SELECT DISTINCT p FROM Product p " +
+                        "JOIN p.sectors s " +
+                        "LEFT JOIN p.brand b " +
+                        "WHERE s.id = :id " +
+                        "AND (:productName IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :productName, '%'))) " +
+                        "AND (:brandId IS NULL OR b.id = :brandId) " +
+                        "AND (:sku IS NULL OR p.sku = :sku) " +
+                        "AND (:ean IS NULL OR p.ean = :ean) " +
+                        "AND (:isActive IS NULL OR p.isActive = :isActive)")
+        Page<Product> findAllProductsBySectorIdWithFilters(
+                        Pageable pageable,
+                        @Param("id") String id,
+                        @Param("productName") String productName,
+                        @Param("sku") String sku,
+                        @Param("ean") String ean,
+                        @Param("brandId") String brandId,
+                        @Param("isActive") boolean isActive);
+
+        @Query("SELECT p FROM Product p " +
+                        "WHERE p.brand.id = :id " +
+                        "AND (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))")
+        Page<Product> findByBrandIdAndName(
+                        Pageable pageable,
+                        @Param("id") String brandId,
+                        @Param("name") String name);
 }
