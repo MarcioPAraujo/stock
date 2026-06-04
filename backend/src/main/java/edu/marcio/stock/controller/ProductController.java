0fcf390eaf2e7c0fb990605cfe143314d41730f7
Marcio <marciopereiraaraujo1997d1@gmail.com>
2026-06-04 17:09:23 -0300

    feat(backend-product): register product endpoint

--- backend/src/main/java/edu/marcio/stock/controller/ProductController.java
+++ backend/src/main/java/edu/marcio/stock/controller/ProductController.java
@@ -3,11 +3,29 @@ package edu.marcio.stock.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.marcio.stock.dto.product.ProductRequest;
+import edu.marcio.stock.dto.product.ProductResponse;
+import edu.marcio.stock.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> registerNewProduct(@Valid @RequestBody ProductRequest body) {
+        ProductResponse response = productService.registerProduct(body);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
 }
